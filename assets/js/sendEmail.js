1879b0cede8b09433b782021177d19a38230d917
Nathan Drake <133364751+OrgNateDrake@users.noreply.github.com>
2023-08-11 18:33:18 +0100

    Maint:Fix emailJS not sending

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_vyco2xk","template_ltzb4p3", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
